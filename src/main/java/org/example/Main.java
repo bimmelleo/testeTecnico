3eaaaa02da9e00b148e86d8c9a4f8aef7bb32abc
bimmelleo <bimmelleo@gmail.com>
2026-04-02 15:18:26 -0300

    feature: postgreSQL start

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,35 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws SQLException {
+        Connection conexao = null;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            conexao = DriverManager.getConnection("jdbc:postgresql://localhost:5432/BancoPostgre","bimmelleo","123456");
+            ResultSet resultado = conexao.createStatement().executeQuery("SELECT * FROM CLIENT");
+            while (resultado.next()) {
+                System.out.println("Nome: " + resultado.getString("name"));
+            }
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Driver do Banco de Dados NÃO LOCALIZADO.");
+        } catch ( SQLException ex) {
+            System.out.println("ERRO de conexão com Banco de Dados: " + ex.getMessage());
+        } finally {
+            if (conexao != null){
+                conexao.close();
+            }
+        }
+
+
 
         //criando o objeto service para conseguir aplicar
         //os métodos de TipoCombService
